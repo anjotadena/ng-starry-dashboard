d516e1e13ba169fbe8eee3a5830ef15a5d39eafc
Anjo Tadena <tadena.anjo@gmail.com>
2024-03-30 00:44:34 +0800

    Refactor app.component and fix not found imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Event, Router, NavigationStart, NavigationEnd, RouterModule } from '@angular/router';
-import { PageLoaderComponent } from './layout/page-loader/page-loader.component';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  Event,
+  NavigationEnd,
+  NavigationStart,
+  Router,
+  RouterModule,
+} from '@angular/router';
 
+import { PageLoaderComponent } from './layout/page-loader/page-loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    PageLoaderComponent,
-
-  ],
+  imports: [CommonModule, RouterModule, PageLoaderComponent],
   providers: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private readonly _router = inject(Router);
+  private readonly _destroyRef = inject(DestroyRef);
+
   currentUrl!: string;
-  constructor(public _router: Router) {
-    this._router.events.subscribe((routerEvent: Event) => {
-      if (routerEvent instanceof NavigationStart) {
-        this.currentUrl = routerEvent.url.substring(
-          routerEvent.url.lastIndexOf('/') + 1
-        );
-      }
-      if (routerEvent instanceof NavigationEnd) {
-        /* empty */
-      }
-      window.scrollTo(0, 0);
-    });
+
+  constructor() {
+    this._resetToEveryRouteEventNavigate();
+  }
+
+  private _resetToEveryRouteEventNavigate(): void {
+    this._router.events
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe((routerEvent: Event) => {
+        if (routerEvent instanceof NavigationStart) {
+          this.currentUrl = routerEvent.url.substring(
+            routerEvent.url.lastIndexOf('/') + 1
+          );
+        }
+
+        if (routerEvent instanceof NavigationEnd) {
+          // empty
+        }
+
+        window.scrollTo(0, 0);
+      });
   }
 }
